e5c42b25e770858ea316d4f56bf685cbbc3e5d94
vominhhieu <mittoleo2911@gmail.com>
2024-06-21 16:33:40 +0700

    header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,14 @@ import {useState} from 'react'
 import UserLogin from './views/UserLogin'
 import LoginHeader from './components/LoginHeader'
 import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
     return (
         <>
+            <Header/>
             <Footer/>
+
         </>
     )
 }
