802c8849a06916e897f5bb8158c85667f9cf14a5
royalishere <{ID}+{username}@users.noreply.github.com>
2024-07-01 15:22:05 +0700

    link images to firebase

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../public/events_logo.jpeg';
+import logo from '/events_logo.jpeg';
 import {useAuth} from '../context/AuthContext';
 import {logout} from '../api/auth';
 
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -8,26 +8,39 @@ import EventPooling from '../components/EventPooling.jsx';
 import EventItem from "../components/EventItem.jsx";
 import EventCard from "../components/EventCard.jsx";
 import {getAllEvents} from "../api/event.js";
+import {getStorage, ref, getDownloadURL} from "firebase/storage";
 
 const Home = () => {
-
+    const firebaseStorage = getStorage();
     const [listEvents, setListEvents] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const getImgList = async (events) => {
+        const promises = events.map((event) => {
+            const imgRef = ref(firebaseStorage, `events/event_${event.id}.jpg.webp`);
+            return getDownloadURL(imgRef);
+        });
+        return Promise.all(promises);
+    };
 
     const handleEvents = async () => {
         try {
             const ListEvents = await getAllEvents();
             if (ListEvents && ListEvents.data) {
+                const image_urls = await getImgList(ListEvents.data);
                 const updatedEvents = ListEvents.data.map((event, index) => ({
                     ...event,
-                    image: `/event_${index + 1}.jpg`,
+                    image: image_urls[index],
                     price: "500.000đ"
                 }));
                 setListEvents(updatedEvents);
             }
         } catch (error) {
             console.log("Error", error);
+        } finally {
+            setLoading(false);
         }
-    }
+    };
 
     useEffect(() => {
         handleEvents();
@@ -39,30 +52,35 @@ const Home = () => {
         <div className="home-page">
             <Header/>
             <NavBar/>
-            <Carousel slides={specialEvents}/>
-            <h5 className="fw-bold ms-4">Sự kiện đặc biệt</h5>
-            <EventPooling Component={EventCard} events={specialEvents}/>
-
-            <h5 className="fw-bold ms-4">Nhạc sống</h5>
-            <EventPooling Component={EventItem} events={listEvents}/>
-            <p className="text-end me-4"><a href="/events" className="more-detail">Xem thêm <i
-                className="bi bi-arrow-right-square"></i></a>
-            </p>
-            <h5 className="fw-bold ms-4">Sân khấu & nghệ thuật</h5>
-            <EventPooling Component={EventItem} events={listEvents}/>
-            <p className="text-end me-4"><a href="/events" className="more-detail">Xem thêm <i
-                className="bi bi-arrow-right-square"></i></a>
-            </p>
-            <h5 className="fw-bold ms-4">Thể thao</h5>
-            <EventPooling Component={EventItem} events={[]}/>
-            <p className="text-end me-4"><a href="/events" className="more-detail">Xem thêm <i
-                className="bi bi-arrow-right-square"></i></a>
-            </p>
-            <h5 className="fw-bold ms-4">Khác</h5>
-            <EventPooling Component={EventItem} events={[]}/>
-            <p className="text-end me-4"><a href="/events" className="more-detail">Xem thêm <i
-                className="bi bi-arrow-right-square"></i></a>
-            </p>
+            {loading ? (
+                <div className="spinner-grow text-success" role="status"></div>
+            ) : (
+                <>
+                    <Carousel slides={specialEvents}/>
+                    <h5 className="fw-bold ms-4">Sự kiện đặc biệt</h5>
+                    <EventPooling Component={EventCard} events={specialEvents}/>
+                    <h5 className="fw-bold ms-4">Nhạc sống</h5>
+                    <EventPooling Component={EventItem} events={listEvents}/>
+                    <p className="text-end me-4"><a href="/events" className="more-detail">Xem thêm <i
+                        className="bi bi-arrow-right-square"></i></a>
+                    </p>
+                    <h5 className="fw-bold ms-4">Sân khấu & nghệ thuật</h5>
+                    <EventPooling Component={EventItem} events={listEvents}/>
+                    <p className="text-end me-4"><a href="/events" className="more-detail">Xem thêm <i
+                        className="bi bi-arrow-right-square"></i></a>
+                    </p>
+                    <h5 className="fw-bold ms-4">Thể thao</h5>
+                    <EventPooling Component={EventItem} events={[]}/>
+                    <p className="text-end me-4"><a href="/events" className="more-detail">Xem thêm <i
+                        className="bi bi-arrow-right-square"></i></a>
+                    </p>
+                    <h5 className="fw-bold ms-4">Khác</h5>
+                    <EventPooling Component={EventItem} events={[]}/>
+                    <p className="text-end me-4"><a href="/events" className="more-detail">Xem thêm <i
+                        className="bi bi-arrow-right-square"></i></a>
+                    </p>
+                </>
+            )}
             <Footer/>
         </div>
     );
